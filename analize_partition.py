b63e114b55a89dd4cb0b1b33c497b349a644a79c
Manolache Mihai <mihaimanolache03@gmail.com>
2024-12-18 12:19:36 +0200

    Added plots containing two subplots about the size and count of each file type

--- analize_partition.py
+++ analize_partition.py
@@ -2,11 +2,38 @@ import os
 from sys import argv
 import matplotlib.pyplot as plt
 
+def plot_chart(extension_size, extension_count):
+    sizes = list(extension_size.items())
+    counts = list(extension_count.items())
+    for i in range(1, len(sizes), 25):
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 10))
+        chunk = sizes[i:i + 25]
+        keys, values = zip(*chunk)
+        values = [v / (1024 * 1024)  for v in values]
+        bars = ax1.bar(keys, values, color='green')
+        ax1.set_title('File Size and Count Comparison by Extension')
+        ax1.set_ylabel('Size (MB)')
+        ax1.set_xticks(range(len(keys)))
+        ax1.set_xticklabels(keys, rotation=25, ha='right')
+        for bar, value in zip(bars, values):
+            ax1.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), f'{round(value, 2)}', ha='center', va='bottom')
+        chunk = counts[i:i + 25]
+        keys, values = zip(*chunk)
+        bars = ax2.bar(keys, values, color='blue')
+        ax2.set_ylabel('Count')
+        ax2.set_xticks(range(len(keys)))
+        ax2.set_xticklabels(keys, rotation=25, ha='right')
+        for bar, value in zip(bars, values):
+            ax2.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), f'{(int)(value)}', ha='center', va='bottom')
+        plt.tight_layout()
+        plt.show()
+
 def analyze_partition(partition):
     extension_count = {}
     extension_size = {}
     total_files = 0
     total_dirs = 0
+    total_size = 0
     for root, dirs, files in os.walk(partition + ":\\"):
         total_dirs += len(dirs)
         total_files += len(files)
@@ -14,6 +41,7 @@ def analyze_partition(partition):
             file_path = os.path.join(root, file)
             file_extension = os.path.splitext(file)[1].lower()
             file_size = os.path.getsize(file_path)
+            total_size += file_size
             if(file_extension not in extension_count):
                 extension_count[file_extension] = 1
                 extension_size[file_extension] = file_size
@@ -24,6 +52,7 @@ def analyze_partition(partition):
     plt.pie([total_dirs, total_files], labels=[f'Directories ({total_dirs})', f'Files ({total_files})'], autopct='%1.1f%%', colors=['blue', 'green'])
     plt.title('Directories and Files')
     plt.show()
+    plot_chart(extension_size, extension_count)
 
 if __name__ == "__main__":
     partition = argv[1]
