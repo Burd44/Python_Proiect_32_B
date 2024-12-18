a37217153805f3ebe82b69cca91e7466b466e964
Manolache Mihai <mihaimanolache03@gmail.com>
2024-12-18 17:41:05 +0200

    Added buttons and navigation

--- analize_partition.py
+++ analize_partition.py
@@ -1,6 +1,14 @@
 import os
 from sys import argv
+import sys
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+
+def quit(event):
+    sys.exit()
+
+def next(event):
+    plt.close()
 
 def plot_chart(extension_size, extension_count):
     sizes = list(extension_size.items())
@@ -10,7 +18,7 @@ def plot_chart(extension_size, extension_count):
         chunk = sizes[i:i + 25]
         keys, values = zip(*chunk)
         values = [v / (1024 * 1024)  for v in values]
-        bars = ax1.bar(keys, values, color='green')
+        bars = ax1.bar(keys, values, color='sienna')
         ax1.set_title('File Size and Count Comparison by Extension')
         ax1.set_ylabel('Size (MB)')
         ax1.set_xticks(range(len(keys)))
@@ -19,13 +27,20 @@ def plot_chart(extension_size, extension_count):
             ax1.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), f'{round(value, 2)}', ha='center', va='bottom')
         chunk = counts[i:i + 25]
         keys, values = zip(*chunk)
-        bars = ax2.bar(keys, values, color='blue')
+        bars = ax2.bar(keys, values, color='royalblue')
         ax2.set_ylabel('Count')
         ax2.set_xticks(range(len(keys)))
         ax2.set_xticklabels(keys, rotation=25, ha='right')
         for bar, value in zip(bars, values):
             ax2.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), f'{(int)(value)}', ha='center', va='bottom')
-        plt.tight_layout()
+        ax_button1 = plt.axes([0.89, 0.01, 0.1, 0.03])
+        button1 = Button(ax_button1, 'Next')
+        button1.on_clicked(next)
+        ax_button2 = plt.axes([0.01, 0.01, 0.1, 0.03])
+        button2 = Button(ax_button2, 'Quit')
+        button2.on_clicked(quit)
+        manager = plt.get_current_fig_manager()
+        manager.full_screen_toggle()
         plt.show()
 
 def analyze_partition(partition):
@@ -52,8 +67,16 @@ def analyze_partition(partition):
                 extension_count[file_extension] += 1
                 extension_size[file_extension] += file_size
     plt.figure(figsize=(10, 5))
-    plt.pie([total_dirs, total_files], labels=[f'Directories ({total_dirs})', f'Files ({total_files})'], autopct='%1.1f%%', colors=['blue', 'green'])
+    plt.pie([total_dirs, total_files], labels=[f'Directories ({total_dirs})', f'Files ({total_files})'], autopct='%1.1f%%', colors=['thistle', 'wheat'])
     plt.title('Directories and Files')
+    ax_button1 = plt.axes([0.89, 0.01, 0.1, 0.03])
+    button1 = Button(ax_button1, 'Next')
+    button1.on_clicked(next)
+    ax_button2 = plt.axes([0.01, 0.01, 0.1, 0.03])
+    button2 = Button(ax_button2, 'Quit')
+    button2.on_clicked(quit)
+    manager = plt.get_current_fig_manager()
+    manager.full_screen_toggle()
     plt.show()
     plot_chart(extension_size, extension_count)
 
@@ -61,5 +84,4 @@ if __name__ == "__main__":
     if len(argv) < 2:
         print("Error: Please provide a partition")
         exit(0)
-    partition = argv[1]
-    analyze_partition(partition)
+    analyze_partition(argv[1])
